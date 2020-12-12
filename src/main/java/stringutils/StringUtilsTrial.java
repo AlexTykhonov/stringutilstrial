40544752ae91edc26ba212d215ff8154bb9e7c56
AlexTykhonov <alexey.tykhonov@gmail.com>
2020-12-12 19:46:05 +0200

    Initial commit

---
+++ src/main/java/stringutils/StringUtilsTrial.java
@@ -0,0 +1,10 @@
+package stringutils;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringUtilsTrial {
+
+    public static void main(String[] args) {
+        System.out.println("9+Reverse is" + StringUtils.reverse("reverse"));
+    }
+}
