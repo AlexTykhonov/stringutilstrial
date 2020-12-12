30dc6b3beec07f02e8ee1f50be8c4efb9d71e5cc
AlexTykhonov <alexey.tykhonov@gmail.com>
2020-12-12 19:52:33 +0200

    updated phrase

--- src/main/java/stringutils/StringUtilsTrial.java
+++ src/main/java/stringutils/StringUtilsTrial.java
@@ -5,6 +5,6 @@ import org.apache.commons.lang3.StringUtils;
 public class StringUtilsTrial {
 
     public static void main(String[] args) {
-        System.out.println("9+Reverse is" + StringUtils.reverse("reverse"));
+        System.out.println("Reverse for 'Deliver Gateman' is " + StringUtils.reverse("Deliver Gateman"+" :)"));
     }
 }
